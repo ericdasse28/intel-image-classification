d5c63806478867ddb4ce58be5e6e2856ff0c9cce
Eric Dasse <christ.gououi@gmail.com>
2024-07-06 10:04:14 +0200

    Update preprocess dataset logic

--- intel_image_classification/preprocess_dataset.py
+++ intel_image_classification/preprocess_dataset.py
@@ -1,37 +1,80 @@
 """Preprocess Intel image classification dataset."""
 
+from dataclasses import dataclass
 import glob
 import os
+import argparse
+from pathlib import Path
 
 import cv2
 from matplotlib import pyplot as plt
 
-from intel_image_classification.dataset_paths import (
-    get_prepared_train_data_dir,
-    get_raw_training_data_dir,
-)
+
+@dataclass
+class Image:
+    name: str
+    content: cv2.typing.MatLike
+    category: str
 
 
-def shrink_dataset_images(new_size: tuple):
-    """Preprocess training dataset."""
+def collect_images(category_folder):
+    """Collect images from category folder"""
+    image_files = glob.glob(pathname=f"{category_folder}/*.jpg")
 
-    raw_train_data_dir = get_raw_training_data_dir()
-
-    for class_folder in os.listdir(f"{raw_train_data_dir}/seg_train"):
-        image_files = glob.glob(
-            pathname=f"{raw_train_data_dir}/seg_train/{class_folder}/*.jpg"
+    return [
+        Image(
+            name=os.path.basename(image_file),
+            content=plt.imread(image_file),
+            category=os.path.basename(category_folder),
         )
-        prepared_image_dir = get_prepared_train_data_dir() / class_folder
-        os.makedirs(prepared_image_dir, exist_ok=True)
+        for image_file in image_files
+    ]
 
-        for image_file in image_files:
-            image = plt.imread(image_file)
-            resized_image = cv2.resize(image, new_size)
 
-            image_file_name = os.path.basename(image_file)
-            prepared_image_path = prepared_image_dir / image_file_name
-            plt.imsave(prepared_image_path, resized_image)
+def preprocess_images(images: list[Image]) -> list[Image]:
+    # Shrinking the images can help saving time during training
+    shrinked_images = [
+        resize_image(
+            image,
+            new_size=(100, 100),
+        )
+        for image in images
+    ]
+
+    return shrinked_images
+
+
+def resize_image(image: Image, new_size: tuple) -> Image:
+    resized_image = Image(
+        image.name, cv2.resize(image.content, new_size), image.category
+    )
+    return resized_image
+
+
+def save_preprocessed_images(images: list[Image], save_dir: os.PathLike):
+    for image in images:
+        preprocessed_image_dir = Path(save_dir) / image.category
+        # Create folder if it doesn't exist
+        os.makedirs(preprocessed_image_dir, exist_ok=True)
+
+        preprocessed_image_path = preprocessed_image_dir / image.name
+        plt.imsave(preprocessed_image_path, image.content)
 
 
 def main():
-    shrink_dataset_images(new_size=(100, 100))
+    parser = argparse.ArgumentParser()
+    dataset_path = parser.add_argument("--dataset-path", "-d")
+    preprocessed_dataset_path = parser.add_argument(
+        "--preproc-dataset-path",
+        "-o",
+    )
+    parser.parse_args()
+
+    category_folders = os.listdir(dataset_path)
+    for category_folder in category_folders:
+        images = collect_images(f"{dataset_path}/{category_folder}")
+        preprocessed_images = preprocess_images(images)
+        save_preprocessed_images(
+            preprocessed_images,
+            preprocessed_dataset_path,
+        )
