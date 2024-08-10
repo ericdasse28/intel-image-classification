e2a4c4199fc91d155b77b8bda342e154b5e0131e
Eric Dasse <christ.gououi@gmail.com>
2024-08-10 09:34:00 +0200

    Add logging to evaluation script

--- intel_image_classification/evaluate.py
+++ intel_image_classification/evaluate.py
@@ -3,6 +3,7 @@ import json
 from pathlib import Path
 
 import joblib
+from loguru import logger
 
 from intel_image_classification.dataset import get_data
 
@@ -26,10 +27,13 @@ def main():
     model_path = args.model_path
     metrics_save_path = args.metrics_save_path
 
+    logger.info("Model evaluation...")
     X_test, y_test = get_testing_data()
     model = load_model(model_path)
     test_loss, test_accuracy = model.evaluate(X_test, y_test)
     metrics = {"accuracy": test_accuracy, "loss": test_loss}
+    logger.info(metrics)
 
+    logger.info(f"Saving metrics to {metrics_save_path}")
     with open(metrics_save_path, "w") as metrics_file:
         json.dump(metrics, metrics_file)
